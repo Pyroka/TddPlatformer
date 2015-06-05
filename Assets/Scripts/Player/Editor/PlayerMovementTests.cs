52a8f7f358c1e1bc60d2fc1da6e05b5bc30d87ad
Andrew Thorpe <pyroka@gmail.com>
2015-06-05 13:15:05 +0100

    Created first test for PlayerMovement, that we should fall-down if not on the ground.

---
+++ Assets/Scripts/Player/Editor/PlayerMovementTests.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Player
+{
+    [TestFixture]
+    [Category("Player")]
+    internal class PlayerMovementTests
+    {
+        [Test]
+        public void ShouldFallIfNotOnGround()
+        {
+            var movement = new PlayerMovement {IsOnGround = false};
+
+            var result = movement.Update();
+            result.y.Should().BeLessThan(0.0f);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class PlayerMovement
+{
+    public bool IsOnGround { get; set; }
+
+    public Vector3 Update()
+    {
+        return Vector3.zero;
+    }
+}
